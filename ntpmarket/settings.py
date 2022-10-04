8fb1fb23574e578c45609461fe9fd98b9fd7e84f
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-10-04 07:20:44 +0300

    dsdff

--- ntpmarket/settings.py
+++ ntpmarket/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'ntpmarket.wsgi.application'
 # DATABASES['default'] = dj_database_url.config()
 
 DATABASES = {
-    # 'default': {
+    'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
